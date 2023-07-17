7344c2ed4e69d4a3d45ee8490136387264582523
Jerome Fung <jerome.fung@gmail.com>
2023-07-17 10:27:31 -0400

    initial implementation of rma

--- pyhot.py
+++ pyhot.py
@@ -8,11 +8,12 @@ Author: Jerome Fung (jfung@ithaca.edu)
 '''
 
 import numpy as np
+import numpy.fft as fft
 # import numba
 
 class SLM(object):
 
-    def __init__(self, nx, ny, px, wavelen, f):
+    def __init__(self, nx, ny, px, wavelen, f, random_seed = None):
         '''
         '''
         self.nx = nx
@@ -26,10 +27,22 @@ class SLM(object):
         self.xs, self.ys = np.mgrid[0:nx, 0:ny]
         self.xs = (self.xs - self.nx/2) * self.px # dimensional
         self.ys = (self.ys - self.ny/2) * self.px
-        
+
+        # 1D array of locations
+        self.xs_r = self.xs.ravel()
+        self.ys_r = self.ys.ravel()
+        self.holo_indices = np.arange(len(self.xs_r))
+
+        if random_seed is None:
+            self.rng = np.random.default_rng()
+        else:
+            self.rng = np.random.default_rng(seed = random_seed)
+
     def calc_holo(self, pts, method = 'spl'):
         if method == 'spl':
             return self._calc_holo_spl(pts)
+        elif method == 'rs':
+            return self._calc_holo_rs(pts)
         else:
             raise NotImplementedError
 
@@ -39,21 +52,77 @@ class SLM(object):
         '''
 
         holo_sum = np.zeros((self.nx, self.ny), dtype = np.complex128)
-        
+
         for pt in pts:
-            Delta_m_lens = np.pi * pt[2] / (self.wavelen * self.f**2) * \
-                (self.xs**2 + self.ys**2)
-            Delta_m_grating = 2 * np.pi / (self.wavelen * self.f) * (
-                self.xs * pt[0] + self.ys * pt[1])
-            Delta_m = Delta_m_lens + Delta_m_grating
-            holo_sum += np.exp(1j * Delta_m)
+            holo_sum += np.exp(1j * self._calc_single_pt_holo(pt))
 
-        # np.angle between -pi and pi, shift to 0 to 2 pi 
-        holo = np.angle(holo_sum) + np.pi
+        return convert_and_scale(holo_sum)
 
-        # scale to 8 bit
-        holo = holo * 255 / (2 * np.pi)
+    def _calc_holo_rs(self, pts):
+        '''
+        Calculate hologram using random superposition, adding a randomly-chosen
+        phase factor to the hologram associated with each point trap.
+        '''
+        holo_sum = np.zeros((self.nx, self.ny), dtype = np.complex128)
 
-        # round
-        return np.ushort(np.around(holo))
-        # return holo
+        random_phases = self.rng.random(len(pts)) * 2 * np.pi
+
+        for pt, phase in zip(pts, random_phases):
+            holo_sum += np.exp(1j * (self._calc_single_pt_holo(pt) + phase))
+
+        return convert_and_scale(holo_sum)
+
+    def _calc_holo_rma(self, pts):
+        '''
+        Calculate holograms using random mask encoding. For N trap points,
+        a randomly-chosen subset of 1/N of the hologram points display
+        the hologram associated with any given trap.
+        '''
+        npts = len(self.xs_r)
+        holo = np.zeros(npts)
+        shuffled_indices = self.rng.permuted(self.holo_indices)
+        npts_over_M = np.floor(npts/len(pts)).astype('int')
+
+        for pt_index, point in zip(np.arange(len(pts)), pts):
+            lower_ind = pt_index * npts_over_M
+            upper_ind = (pt_index + 1) * npts_over_M
+            holo[shuffled_indices[lower_ind:upper_ind]] = \
+                _calc_single_pt_holo(point, xs[lower_ind:upper_ind],
+                                     ys[lower_ind:upper_ind])
+
+        return convert_and_scale(holo.reshape((self.nx, self.ny)))
+
+
+    def _calc_single_pt_holo(self, pt, xs = None, ys = None):
+        '''
+        Grating-and-lens hologram to produce a single point trap.
+        '''
+        if xs is None:
+            xs = self.xs
+        if ys is None:
+            ys = self.ys
+
+        Delta_m_lens = np.pi * pt[2] / (self.wavelen * self.f**2) * \
+            (xs**2 + ys**2)
+        Delta_m_grating = 2 * np.pi / (self.wavelen * self.f) * (
+            xs * pt[0] + ys * pt[1])
+        return Delta_m_lens + Delta_m_grating
+
+
+def convert_and_scale(raw_holo):
+    # np.angle between -pi and pi, shift to 0 to 2 pi
+    holo = np.angle(raw_holo) + np.pi
+    # scale to 8 bit
+    holo = holo * 255 / (2 * np.pi)
+    # round
+    return np.ushort(np.around(holo))
+
+
+def calc_fourier_amplitude(holo):
+    '''
+    Utility function to calculate the field in the trapping plane
+    that would be produced by a given hologram.
+    '''
+    phase = holo * (2 * np.pi) / 255
+    field = np.exp(1j*phase)
+    return np.abs(fft.fftshift(fft.fft2(field)))**2
