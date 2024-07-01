298b1f61875f1c5013e38ccc080ea658b03bba63
unknown <jerome.fung@gmail.com>
2024-07-01 10:14:24 -0400

    basic save works modified code to adjust exposure time of camera

--- slm_gui.py
+++ slm_gui.py
@@ -6,6 +6,7 @@ import pyhot
 import os
 import glob
 import yaml 
+import time
 
 from thorlabs_tsi_sdk.tl_camera import TLCameraSDK
 
@@ -79,9 +80,118 @@ class appFrame(wx.Frame):
         super().__init__(parent = parent, title = title, pos = pos, size = size )
         self.camera_object = camera_object
         self.OnInit()
+        self.InitUI()
 
     def OnInit(self):
         guiPanel = appPanel(parent=self, camera_object = self.camera_object)
+        
+    def InitUI(self):
+        self.CreateMenuBar()
+        self.panel = wx.Panel(self)
+
+    def CreateMenuBar(self):
+        menubar = wx.MenuBar()
+
+        fileMenu = wx.Menu()
+        fileMenu.Append(wx.ID_EXIT, 'Quit', 'Quit application')
+        
+        saveSubMenu = wx.Menu()
+        
+        saveSubMenu.Append(201, 'Save sequence of images', 'Save sequence of images')
+        fileMenu.AppendSubMenu(saveSubMenu, 'Save')
+       
+       
+
+        menubar.Append(fileMenu,'&File')
+        
+
+        settingsMenu = wx.Menu()
+        exposureMenu = wx.Menu()
+        exposureMenu.Append(101, 'Set Exposure Time', 'Set exposure time')
+        settingsMenu.AppendSubMenu(exposureMenu, 'Exposure Time')
+        menubar.Append(settingsMenu, '&Settings')
+
+        self.Bind(wx.EVT_MENU, self.OnQuit, id=wx.ID_EXIT)
+        self.Bind(wx.EVT_MENU, self.OnSetExposure, id=101)
+        self.Bind(wx.EVT_MENU, self.OnSaveSequence, id = 201)
+        self.SetMenuBar(menubar)
+    
+    
+    def OnSaveSequence(self,e):
+        dlg = wx.DirDialog(self, "Choose file directory to save images",style = wx.DD_DEFAULT_STYLE)
+        
+        if dlg.ShowModal() == wx.ID_OK:
+          path = dlg.GetPath()
+          dlg.Destroy()
+          
+          
+          dialog = wx.Dialog(self, title = "Save sequence of images")
+          
+          wx.StaticText(dialog,label = "Folder Path", pos = (10,10))
+          path_text = wx.TextCtrl(dialog, value = path, pos = (120,10), size = (100,-1), style =wx.TE_READONLY)
+          
+          wx.StaticText(dialog, label = "File name",pos = (10,40))
+          filename_text = wx.TextCtrl(dialog, pos = (120,40), size = (200,-1))
+          
+          wx.StaticText(dialog, label = "Number of images", pos = (10,70))
+          num_images = wx.SpinCtrl(dialog, value = '1', pos = (120,70), size = (80, -1), min = 1, max = 2000)
+          
+          
+          wx.StaticText(dialog, label = "Frame rate", pos = (10,100))
+          frame_rate = wx.TextCtrl(dialog, pos = (120,100))
+          
+          btn_ok = wx.Button(dialog, wx.ID_OK, "Start Acquisition", pos = (120, 150))
+          btn_cancel = wx.Button(dialog, wx.ID_CANCEL, "Cancel", pos = (250,150))
+          
+          
+          btn_ok.Bind(wx.EVT_BUTTON, lambda event: self.OnStartAcquisition(event, path_text.GetValue(), filename_text.GetValue(), num_images.GetValue(), frame_rate.GetValue()))
+          
+          
+          
+          if dialog.ShowModal() ==wx.ID_OK:
+       
+            filename = filename_text.GetValue()
+            num_images = num_images.GetValue()
+        
+        
+      
+    def OnStartAcquisition(self,e,path,filename,num_images,frame_rate):
+        frame_interval = 1.0/float(frame_rate)
+        
+        for i in range(num_images):
+          self.camera_object.camera.issue_software_trigger()
+          frame =self.camera_object.camera.get_pending_frame_or_null()
+          if frame is not None:
+         
+            image_data = np.copy(frame.image_buffer)
+            pil_image = im.fromarray(image_data)
+          
+            image_filename = f"{filename}_{i+1}.png"
+            image_filepath=os.path.join(path, image_filename)
+          
+            pil_image.save(image_filepath)
+            print(f"Saved image {i+1}/{num_images} to {image_filepath}")
+            time.sleep(frame_interval)
+          
+        
+          
+    def OnQuit(self, e):
+        self.Close()
+
+    def OnSetExposure(self, e):
+        current_exposure_time = self.camera_object.camera.exposure_time_us / 1000
+        exposure_dialog = wx.TextEntryDialog(self, f'Enter exposure time in ms (currently at {current_exposure_time}ms): ','Set Exposure Time')
+        if exposure_dialog.ShowModal() == wx.ID_OK  and exposure_dialog.GetValue().isdigit():
+            exposure_time = int(exposure_dialog.GetValue()) 
+            self.camera_object.camera.exposure_time_us = exposure_time * 1000
+            
+            #set exposure time to the value we get
+            print(f'Exposure time set to {exposure_time} ms')
+            
+        else:
+            print("Use a non-zero integer") #wx.MessageBox("Invalid input. Please enter a non-zero integer.", "Error", wx.OK | wx.ICON_ERROR)
+            
+        exposure_dialog.Destroy()
 
 class appPanel(wx.Panel):
     def __init__(self,parent, camera_object):
@@ -325,7 +435,7 @@ class hologram(wx.Frame):
 def scale_bitmap(bitmap,ratio):
     # image = wx.ImageFromBitmap(bitmap)
     image =  bitmap.ConvertToImage()
-    image = image.Scale(round(bitmap.GetWidth()*ratio), round(bitmap.GetHeight()*ratio), wx.IMAGE_QUALITY_HIGH)
+    image = image.Scale(round(bitmap.GetWidth()*0.4), round(bitmap.GetHeight()*0.4), wx.IMAGE_QUALITY_HIGH)
     # image = image.Scale(width, height, wx.IMAGE_QUALITY_HIGH)
     result = wx.Bitmap(image)
     return result
