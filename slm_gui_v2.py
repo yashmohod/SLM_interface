82fc7fb610e1efe5ef5c1618bcb79545cd5d6acd
Jerome Fung <jerome.fung@gmail.com>
2024-07-12 10:59:49 -0400

    properly close camera on main window close

--- slm_gui_v2.py
+++ slm_gui_v2.py
@@ -66,7 +66,8 @@ class MainApp(wx.App):
         main_window_size = (self.main_window_scale_factor * 
                             np.array(main_display_geo[2:4])).round().astype('int')
         return MainWindowFrame(pos = main_window_pos,
-                               size = main_window_size)
+                               size = main_window_size,
+                               camera_object = self.camera_object)
         
         
     def init_slm_window(self):
@@ -74,9 +75,15 @@ class MainApp(wx.App):
         
 
 class MainWindowFrame(wx.Frame):
-    def __init__(self, pos, size):
+    def __init__(self, pos, size, camera_object):
         super().__init__(parent = None, title = "SLM and Camera Control", 
                          pos = pos, size = size)
+        self.camera_object = camera_object
+        self.Bind(wx.EVT_CLOSE, self.on_close)
+    
+    def on_close(self, event):
+        self.camera_object._cleanup()
+        self.Destroy()
         
 
 if __name__ == "__main__":
